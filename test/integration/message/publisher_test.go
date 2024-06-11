38f1b0d3b1d781c1a574abcf25019bdb5bcd1fd4
Mykola Guley <mykolaguley99@gmail.com>
2024-06-11 11:50:37 +0300

    implemented consumer integration tests, code refactoring

--- test/integration/message/publisher_test.go
+++ test/integration/message/publisher_test.go
@@ -1,165 +1,100 @@
 package message
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/mguley/web-scraper-v1/internal/message/publisher"
-	"github.com/mguley/web-scraper-v1/internal/message/shared"
 	"github.com/mguley/web-scraper-v1/internal/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"sync"
 	"testing"
-	"time"
 )
 
-// setupRabbitMQ initializes a RabbitMQ connection, purges the queue, and creates a new RabbitMQPublisher instance.
-// It returns the RabbitMQPublisher instance and a cleanup function to close the connection and channel.
+// setupPublisher initializes a RabbitMQPublisher for publishing messages.
+// It also purges any messages that might be lingering in the queue to ensure tests start clean.
+// Returns the publisher instance along with a cleanup function to properly release resources after tests.
 //
 // Parameters:
-// - t *testing.T: The testing framework's instance.
+// - t *testing.T: The testing framework's instance, used for making assertions.
+// - deleteQueue bool: A flag that indicates if we need to drop a queue.
 //
 // Returns:
-// - *publisher.RabbitMQPublisher[model.Job]: The initialized RabbitMQPublisher instance.
-// - func(): A cleanup function to close the RabbitMQ connection and channel.
-func setupRabbitMQ(t *testing.T) (*publisher.RabbitMQPublisher[model.Job], func()) {
-	config := cfg.RabbitMQ
-	connString := fmt.Sprintf("amqp://%s:%s@%s:%s", config.User, config.Pass, config.Host, config.Port)
-	connection, channel, err := shared.SetupRabbitMQ(connString, config.QueueName)
-	require.NoError(t, err, "Failed to setup RabbitMQ")
-
-	cleanup := func() {
-		require.NoError(t, connection.Close(), "Failed to close connection")
-		require.NoError(t, channel.Close(), "Failed to close channel")
-	}
-
-	// clean up queue
-	_, purgeErr := channel.QueuePurge(config.QueueName, false)
-	require.NoError(t, purgeErr, "Failed to purge RabbitMQ queue")
-
-	// create a new publisher instance
+// - *publisher.RabbitMQPublisher[model.Job]: The initialized publisher.
+// - func(): A cleanup function to close the connection and delete the queue.
+func setupPublisher(t *testing.T, deleteQueue bool) (*publisher.RabbitMQPublisher[model.Job], func()) {
 	factory := publisher.Factory[model.Job]{}
-	pub, err := factory.NewPublisher(cfg.RabbitMQ)
-	require.NoError(t, err)
+	pub, pubErr := factory.NewPublisher(cfg.RabbitMQ)
+	require.NoError(t, pubErr)
 	require.NotNil(t, pub)
 
-	return pub.(*publisher.RabbitMQPublisher[model.Job]), cleanup
+	publisherRabbitMQ := pub.(*publisher.RabbitMQPublisher[model.Job])
+
+	// Clean up queue
+	_, purgeErr := publisherRabbitMQ.Channel.QueuePurge(cfg.RabbitMQ.QueueName, false)
+	require.NoError(t, purgeErr)
+
+	releaseResources := func() {
+		if deleteQueue {
+			_, delErr := publisherRabbitMQ.Channel.QueueDelete(cfg.RabbitMQ.QueueName, false, false, false)
+			require.NoError(t, delErr)
+		}
+		require.NoError(t, publisherRabbitMQ.Close())
+	}
+
+	return publisherRabbitMQ, releaseResources
 }
 
-// TestPublishMessage verifies that a message is published to RabbitMQ successfully.
-// It publishes test messages and checks if they are correctly added to the queue.
+// TestPublishMessages verifies that messages can be published concurrently to a RabbitMQ queue.
+// It sets up a publisher, sends a large number of messages, and ensures all are successfully added.
 //
 // Parameters:
 // - t *testing.T: The testing framework's instance.
-func TestPublishMessage(t *testing.T) {
-	pub, cleanup := setupRabbitMQ(t)
-	defer cleanup()
-
-	tests := []struct {
-		name    string
-		message model.Job
-	}{
-		{"Test Message", model.Job{Title: "Test Message", URL: "https://www.example.com"}},
-		{"Test Message 123", model.Job{Title: "Test Message 123", URL: "https://www.example.com"}},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			publishErr := pub.Publish(tt.message)
-			assert.NoError(t, publishErr)
-
-			channel, channelErr := pub.Connection.Channel()
-			require.NoError(t, channelErr)
-			defer func() {
-				require.NoError(t, channel.Close(), "Failed to close channel")
-			}()
-
-			msg, ok, getErr := channel.Get(cfg.RabbitMQ.QueueName, true)
-			require.NoError(t, getErr)
-			require.True(t, ok)
-
-			var receivedMessage model.Job
-			unmarshalErr := json.Unmarshal(msg.Body, &receivedMessage)
-			require.NoError(t, unmarshalErr)
-			assert.Equal(t, tt.message, receivedMessage)
-		})
-	}
-}
-
-// TestConcurrency verifies that messages are published to RabbitMQ concurrently.
-// It publishes a specified number of messages concurrently and verifies the total count of messages in the queue.
-//
-// Parameters:
-// - t *testing.T: The testing framework's instance.
-func TestConcurrency(t *testing.T) {
-	pub, cleanup := setupRabbitMQ(t)
-	defer cleanup()
+func TestPublishMessages(t *testing.T) {
+	pub, releaseResources := setupPublisher(t, true)
+	defer releaseResources()
+	require.NotNil(t, pub)
 
 	var waitGroup sync.WaitGroup
-	messageCount := 100 // Number of messages to publish concurrently
+	messageCount := 10_000 // Number of messages to publish concurrently
+	list := make([]model.Job, messageCount)
+
+	// Start publishing messages concurrently
 	waitGroup.Add(messageCount)
 	for i := 0; i < messageCount; i++ {
-		go func(msgNumber int) {
+		go func(id int) {
 			defer waitGroup.Done()
-			testMessage := model.Job{Title: "Test Message", URL: "https://www.example.com"}
-			publishErr := pub.Publish(testMessage)
-			assert.NoError(t, publishErr)
+			item := model.Job{Title: fmt.Sprintf("message payload %d", id), URL: "https://www.ex.com"}
+			pubErr := pub.Publish(item)
+			require.NoError(t, pubErr)
+			list[id] = item
 		}(i)
 	}
 	waitGroup.Wait()
-
-	// Verify the number of messages in the queue
-	channel, channelErr := pub.Connection.Channel()
-	require.NoError(t, channelErr)
-	defer func() {
-		require.NoError(t, channel.Close(), "Failed to close channel")
-	}()
-
-	messages, consumeErr := channel.Consume(
-		cfg.RabbitMQ.QueueName,
-		"",    // consumer tag
-		true,  // auto-ack
-		false, // exclusive
-		false, // no-local
-		false, // no-wait
-		nil,   // arguments
-	)
-	require.NoError(t, consumeErr)
-
-	var receivedMessages []model.Job
-	for i := 0; i < messageCount; i++ {
-		select {
-		case message := <-messages:
-			var receivedMessage model.Job
-			unmarshalErr := json.Unmarshal(message.Body, &receivedMessage)
-			require.NoError(t, unmarshalErr)
-			receivedMessages = append(receivedMessages, receivedMessage)
-		case <-time.After(5 * time.Second):
-			t.Fatalf("Timeout while waiting for messages")
-		}
-	}
-
-	require.Len(t, receivedMessages, messageCount, "The number of messages received should be equal to the number of messages sent")
+	require.Equal(t, messageCount, len(list))
 }
 
-// TestCloseFunction verifies that the Close function works correctly.
-// It ensures that the connection and channel are properly closed without errors.
+// TestCloseFunctionPublisher verifies the proper closure of RabbitMQ connections and channels used by the publisher.
+// It checks that the publisher's cleanup operations (closing connections and deleting queues) are error-free.
 //
 // Parameters:
 // - t *testing.T: The testing framework's instance.
-func TestCloseFunction(t *testing.T) {
-	pub, cleanup := setupRabbitMQ(t)
-	defer cleanup()
+func TestCloseFunctionPublisher(t *testing.T) {
+	pub, _ := setupPublisher(t, true)
+	require.NotNil(t, pub)
 
-	// Check that the connection and channel are open
+	// Ensure that the RabbitMQ connection and channel are properly opened
 	require.NotNil(t, pub.Connection, "RabbitMQ connection should be established")
 	require.NotNil(t, pub.Channel, "RabbitMQ channel should be established")
 
-	// Close the publisher
+	// Attempt to delete the queue and verify it is handled without errors
+	_, delErr := pub.Channel.QueueDelete(cfg.RabbitMQ.QueueName, false, false, false)
+	require.NoError(t, delErr, "Failed to delete queue")
+
+	// Close the publisher and ensure there are no errors
 	closeErr := pub.Close()
 	assert.NoError(t, closeErr, "Closing the publisher should not produce an error")
 
-	// Check that the connection and channel are closed
+	// Check that the connection and channel are no longer operational
 	connErr := pub.Connection.Close()
 	assert.Error(t, connErr, "The connection should already be closed")
 	channelErr := pub.Channel.Close()
