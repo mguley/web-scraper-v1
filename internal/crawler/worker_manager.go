1556dd58ff0f50328414cbbc77143837911f049e
Mykola Guley <mykolaguley99@gmail.com>
2024-06-13 16:50:28 +0300

    Code refactoring, affected dispatcher and worker manager

--- internal/crawler/worker_manager.go
+++ internal/crawler/worker_manager.go
@@ -11,8 +11,8 @@ import (
 //   - T any: The type of data that the workers process. This ensures type safety across the worker processes.
 //
 // Fields:
-// - WorkerQueue chan chan Unit: Channel of channels, used to manage available worker queues.
-// - MaxWorkers int: The maximum number of workers that can be managed.
+// - WorkerQueue chan chan Unit: Channel of worker channels, used to distribute available workers.
+// - config DispatcherConfig: Configuration settings for managing worker operations.
 // - UnitQueue chan Unit: Channel for receiving units of work to be processed.
 // - BatchDone chan bool: Channel for signaling the completion of processing a batch of units.
 // - batchWaitGroup sync.WaitGroup: Synchronizes the completion of processing batches.
@@ -20,7 +20,7 @@ import (
 // - Processor processor.Processor[T]: Processor used for handling units of work.
 type WorkerManager[T any] struct {
 	WorkerQueue    chan chan Unit
-	MaxWorkers     int
+	config         DispatcherConfig
 	UnitQueue      chan Unit
 	BatchDone      chan bool
 	batchWaitGroup sync.WaitGroup
@@ -28,29 +28,29 @@ type WorkerManager[T any] struct {
 	Processor      processor.Processor[T]
 }
 
-// NewWorkerManager creates a new WorkerManager instance with a specified maximum number of workers and a processor.
-// This constructor sets up the necessary channels and initializes the list of workers.
+// NewWorkerManager creates a new WorkerManager instance with a specified configuration and a processor.
+// This constructor sets up the necessary channels and initializes the list of workers based on the configuration.
 //
 // Parameters:
-// - maxWorkers int: The maximum number of workers to manage.
+// - config DispatcherConfig: Configuration settings including the maximum number of workers and batch limit.
 // - processor processor.Processor[T]: The processor used for handling units of work.
 //
 // Returns:
 // - *WorkerManager[T]: A pointer to an instance of WorkerManager.
-func NewWorkerManager[T any](maxWorkers int, processor processor.Processor[T]) *WorkerManager[T] {
+func NewWorkerManager[T any](config DispatcherConfig, processor processor.Processor[T]) *WorkerManager[T] {
 	return &WorkerManager[T]{
-		WorkerQueue: make(chan chan Unit, maxWorkers),
-		MaxWorkers:  maxWorkers,
+		WorkerQueue: make(chan chan Unit, config.MaxWorkers),
+		config:      config,
 		UnitQueue:   make(chan Unit),
-		BatchDone:   make(chan bool, maxWorkers),
-		workers:     make([]*Worker[T], 0, maxWorkers),
+		BatchDone:   make(chan bool, config.MaxWorkers),
+		workers:     make([]*Worker[T], 0, config.MaxWorkers),
 		Processor:   processor,
 	}
 }
 
 // Start initializes and starts all worker instances under management.
 func (workerManager *WorkerManager[T]) Start() {
-	for i := 0; i < workerManager.MaxWorkers; i++ {
+	for i := 0; i < workerManager.config.MaxWorkers; i++ {
 		worker := NewWorker[T](i, workerManager.WorkerQueue, workerManager.Processor, workerManager.BatchDone)
 		worker.Start()
 		workerManager.workers = append(workerManager.workers, worker)
@@ -64,9 +64,10 @@ func (workerManager *WorkerManager[T]) AssignUnit(unit Unit) {
 }
 
 // WaitForBatchCompletion waits for the completion of processing for all units in the current batch.
+// This method synchronizes on the batch limit defined in the configuration.
 func (workerManager *WorkerManager[T]) WaitForBatchCompletion() {
-	workerManager.batchWaitGroup.Add(batchLimit)
-	for i := 0; i < batchLimit; i++ {
+	workerManager.batchWaitGroup.Add(workerManager.config.BatchLimit)
+	for i := 0; i < workerManager.config.BatchLimit; i++ {
 		<-workerManager.BatchDone
 		workerManager.batchWaitGroup.Done()
 	}
